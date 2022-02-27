6769e417d69d817e008447c6f3377bf52a377dbf
Yev <tjs_694@hotmail.com>
2022-02-27 18:33:39 +0100

    Added new TODO comment

--- PagesTheme/content.js
+++ PagesTheme/content.js
@@ -1,5 +1,6 @@
 //TODO: Crear un botón para habilitar y deshabilitar la extensión
 //TODO: Crear una forma de lista para excluir las páginas que quiera, posiblemente con chrome.storage.sync.get/set para guardar la lista (con un objeto al que se le vaya appendeando cada URL deseada)
+//TODO: Crear una forma para excluir zonas de una página
 
 let excludedHTML = /^(button|img|h\d)$/gi;
 
